2fa8bf4332f5057dbd6c7f9c911addeda0039437
Mukul Joshi <mukul.joshi@skill-mine.com>
2025-12-17 16:01:52 +0530

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,8 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-
-         <div class="card" styles="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-                                                                
-<div class="card " style="width: 18rem;">
-    <div class="card-header d-flex justify-content-between border-bottom-0">
-        <h6 class="card-title m-0">Header</h6>
-        <i class="fa fa-ellipsis-v"></i>
-    </div>
-     <img src="..." alt="img" />
-    <div class="card-body ">
-        <h5 class="card-title mb-2">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-    <div class="card-footer border-top-0 d-flex justify-content-end gap-3">
-        <button class="btn btn-outline-primary">Cancel</button>
-        <button class="btn btn-primary">Button</button>
-    </div>
-</div>
-
-<div class="card shadow-xl" style="width: 18rem;">
-    <div class="card-header d-flex justify-content-between border-bottom-0">
-        <h6 class="card-title m-0">Header</h6>
-        <i class="fa fa-ellipsis-v"></i>
-    </div>
-     <img src="..." alt="img" />
-    <div class="card-body">
-        <h5 class="card-title mb-2">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-    <div class="card-footer border-top-0 d-flex justify-content-end gap-3">
-        <button class="btn btn-outline-primary">Cancel</button>
-        <button class="btn btn-primary">Button</button>
-    </div>
-</div>
-
-
-    </div>
+    <div>App</div>
   )
 }
 
