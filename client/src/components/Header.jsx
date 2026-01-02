4d121dfa2f7ac79eead07bda3df02c47af5c430b
Mukul Joshi <mukul.joshi@skill-mine.com>
2026-01-02 10:57:53 +0530

    add role access resgistration

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,27 +10,11 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
-<<<<<<< HEAD
-import { NavLink } from "react-router-dom";
-import { NAV_ITEMS } from "./comman/navConfig";
-// import RegisterModal from "./auth/RegisterModal";
-import AuthContainer from "./auth/AuthContainer";
-
-const HEADER_HEIGHT = 64;
-
-const Header = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const [openMenu, setOpenMenu] = useState(false);
-  // const [showRegister, setShowRegister] = useState(false);
-  const [openAuth, setOpenAuth] = useState(false);
-=======
 import { getUser, logout } from "../utils/auth";
 
 const Header = () => {
   const user = getUser();
   const [anchorEl, setAnchorEl] = useState(null);
->>>>>>> origin/HemantNewFeature
 
   return (
     <AppBar
@@ -52,46 +36,9 @@ const Header = () => {
                 <Typography fontSize={12} opacity={0.8}>{user.role}</Typography>
               </Box>
 
-<<<<<<< HEAD
-          <Box ml="auto" display="flex" gap={1}>
-            <Button
-              variant="contained"
-              size="small"
-              sx={{ bgcolor: "#e56712", textTransform: "none" }}
-            >
-              Publish
-            </Button>
-            <Avatar sx={{ width: 32, height: 32 }} />
-
-              {/* <Button
-        size="small"
-        variant="contained"
-        sx={{ bgcolor: "#e56712", textTransform: "none" }}
-        onClick={() => setShowRegister(true)}
-      >
-        Register
-      </Button> */}
-       <Button
-            variant="contained"
-            size="small"
-            sx={{ bgcolor: "#e56712" }}
-            onClick={() => setOpenAuth(true)}
-          >
-            Register
-          </Button>
-          </Box>
-          
-        </Toolbar>
-        {/* {showRegister && (
-        <RegisterModal setShowModal={setShowRegister} />
-      )} */}
-      {openAuth && <AuthContainer onClose={() => setOpenAuth(false)} />}
-      </AppBar>
-=======
               <IconButton onClick={(e) => setAnchorEl(e.currentTarget)}>
                 <Avatar>{user.first_name?.[0]}</Avatar>
               </IconButton>
->>>>>>> origin/HemantNewFeature
 
               <Menu
                 anchorEl={anchorEl}
